4954f79005044b7b5c7fec4400b8a60774f2b216
Sensanaty <salevic@lu-ka.me>
2026-03-21 14:12:05 +0100

    Create main `Mod.cs` file containing Melon init

---
+++ LongLastingFertilizer/Mod.cs
@@ -0,0 +1,15 @@
+﻿using MelonLoader;
+
+[assembly: MelonInfo(typeof(LongLastingFertilizer.Mod), "LongLastingFertilizer", "1.0.0", "Sensanaty")]
+[assembly: MelonGame("TVGS", "Schedule I")]
+
+namespace LongLastingFertilizer {
+  public class Mod : MelonMod {
+    internal static Mod Instance { get; private set; } = null;
+
+    public override void OnInitializeMelon() {
+      Instance = this;
+      LoggerInstance.Msg("[LongLastingFertilizer] Initialized");
+    }
+  }
+}
