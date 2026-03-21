9aaae1383d696c9c6e7f7ce5d23eaa7fe71a5a08
Sensanaty <salevic@lu-ka.me>
2026-03-21 14:18:49 +0100

    Create `Models.cs` for state JSON serialization later

---
+++ LongLastingFertilizer/Models.cs
@@ -0,0 +1,28 @@
+﻿namespace LongLastingFertilizer;
+
+[Serializable]
+public class FertilizerEffect {
+  public string Name { get; set; } = "";
+  public float YieldMultiplier { get; set; }
+  public float QualityChange { get; set; }
+  public float InstantGrowth { get; set; }
+}
+
+[Serializable]
+public class PotFertilizerData {
+  public List<FertilizerEffect> Effects { get; set; } = [];
+  public float YieldLevel { get; set; }
+  public float QualityLevel { get; set; }
+  public bool HasSpeedGrow { get; set; }
+}
+
+[Serializable]
+public class FertilizerSaveFile {
+  public List<FertilizerSaveEntry> Entries { get; set; } = [];
+}
+
+[Serializable]
+public class FertilizerSaveEntry {
+  public string PotId { get; set; } = "";
+  public PotFertilizerData Data { get; set; } = new();
+}
